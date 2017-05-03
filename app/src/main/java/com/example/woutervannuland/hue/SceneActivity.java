12c2796ab037b94ff6a1616c51fa66e64b63ba3c
Wouter van Nuland <wouter.van.nuland@fourtress.nl>
2017-05-04 00:04:42 +0200

    Added SceneActivity

---
+++ app/src/main/java/com/example/woutervannuland/hue/SceneActivity.java
@@ -0,0 +1,74 @@
+package com.example.woutervannuland.hue;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.philips.lighting.hue.sdk.PHHueSDK;
+import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHLight;
+
+import java.util.List;
+
+public class SceneActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private PHBridge verbondenBridge;
+    private List<PHLight> connectedHueList;
+
+    TextView connectedIpTextView;
+    TextView connectedAmountOfLampsTextView;
+
+    Button azianGreenButton;
+    Button springBlossomButton;
+    Button sunsetSceneButton;
+    Button savannahSceneButton;
+
+
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scene);
+
+         azianGreenButton = (Button) findViewById(R.id.azianGreenButton);
+         azianGreenButton.setOnClickListener(this);
+
+         springBlossomButton = (Button) findViewById(R.id.springBlossomButton);
+         springBlossomButton.setOnClickListener(this);
+
+         sunsetSceneButton = (Button) findViewById(R.id.sunsetSceneButton);
+         sunsetSceneButton.setOnClickListener(this);
+
+         savannahSceneButton = (Button) findViewById(R.id.savannahSceneButton);
+         savannahSceneButton.setOnClickListener(this);
+
+         connectedIpTextView = (TextView) findViewById(R.id.connectedIpTextView);
+         connectedAmountOfLampsTextView = (TextView) findViewById(R.id.connectedAmountOfLampsTextView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // hier heb je de bridge weer
+        verbondenBridge = PHHueSDK.getInstance().getSelectedBridge();
+        String connectedIP = verbondenBridge.getResourceCache().getBridgeConfiguration().getIpAddress();
+        connectedIpTextView.setText(("Ip-adres: " + connectedIP));
+
+        // en hier alle lampen
+        List<PHLight> allLights = verbondenBridge.getResourceCache().getAllLights();
+        connectedHueList = allLights;
+        int tmp = connectedHueList.size();
+        connectedAmountOfLampsTextView.setText("Verbonden met " + tmp + " lampen..");
+    }
+
+
+     @Override
+     public void onClick(View view) {
+         //// TODO: als er geklikt wordt dan moet de scene openen en dan moet men de brightness kunnen schalen.
+     }
+ }
